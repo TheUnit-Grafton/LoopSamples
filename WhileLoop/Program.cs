739a07e9cf72e7eeef9b602cfaa3e30e7ba4bfb6
Steve Teece <sgteece@hotmail.com>
2021-02-20 09:22:47 +1100

    Add project files.

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            while (i < 15)
+            {
+                Console.WriteLine("The value of i is " + i);
+                if (i == 13)
+                {
+                    Console.WriteLine("The value of i is unlucky 13. Time for me to stop.");
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+}
